c3134e91340e6520904e3a3a1adc5dd9ba034649
manoj-mmw <manoj@makemywin.com>
2022-02-11 09:57:02 +0530

    db config

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, password, **extra_fields):
+    def create_superuser(self, email, password, **extra_fields):
         """Creates super user"""
         if not email:
             raise ValueError("email is require")
